9daad79ae23b6c1291e0d4024a90bd2a127d9585
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-10 18:56:05 +0530

    test

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 app.get('/:id',async(req,res)=>{
-    return res.status(200).json({message:'This url is not registered to redirect to any url'});
     const url=await urlModel.findById(req.params.id);
     if(!url){
         return res.status(200).json({message:'This url is not registered to redirect to any url'});
